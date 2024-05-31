6d4c3bb9f1a144a624efb8df4756ce0bdf6d05ff
ranumalviya <ranumalviya348@gmail.com>
2024-05-31 14:25:39 +0530

    first v1 of list.py

---
+++ list.py
@@ -0,0 +1,61 @@
+#name="paul"
+user_list =['ranu','shivani','jyoti','sakshi']
+print(user_list)#list print krne ke liye
+print(user_list[0])#indexing position se list ki value print krne ke liye
+print(user_list[1])
+print(user_list[2])
+print(user_list.append('Megha'))#list main value add krne ke liye
+print(user_list)
+print(user_list.remove('Megha'))#list se value delete krne ke liye
+print(user_list)
+user_list[2]='Jyoti'#list me value modify krne ke liye
+print(user_list)
+user_list.insert(3,'shanu') #adding item to a list at given position
+print(user_list)
+del user_list[3]  #delete item to a list at given position
+print(user_list)
+
+#number of item in a list 
+print(len(user_list))
+
+# Sortin of List
+print(user_list)
+
+
+user_list.pop()
+print(user_list)
+
+user_list.sort(reverse=True)
+print(user_list)
+
+user_list.reverse()
+print(user_list)
+
+user_list.pop()
+print(user_list)
+
+removed_list=user_list.pop(0)
+print(removed_list)
+
+print(user_list)
+
+
+#Slicing List
+
+
+user_list2=["apple","banana","chiku","coustardapple"]
+print(user_list2[:2])
+print(user_list2[1:3])
+print(user_list2[-2:])
+print(user_list2[:-2])
+
+
+#Numeric Lists
+marks =[89,98,76,54,67]
+print(marks)
+print(min(marks))
+print(max(marks))
+print(sum(marks))
+
+mix_List=["ranu",2002,18,"malviya"]
+print(mix_List)
